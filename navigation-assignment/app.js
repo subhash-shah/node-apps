daf0b1acbb2dfc906f32d2fd6c993622fc06d495
subhash-shah <shubh4124u@gmail.com>
2020-01-03 19:39:31 +0530

    Added common prefix to the routes.

--- navigation-assignment/app.js
+++ navigation-assignment/app.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(mainRoutes);
 
+// In admin.js, modules.exports = router;
+// const adminRoutes = require('./routes/admin');
+// app.use('/admin', adminRoutes);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
